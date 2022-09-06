e428572fab320011c8211f06db9cf00cae5b68b7
Harvel218 <gacekrzysztof.218@gmail.com>
2022-09-06 16:45:18 +0200

    initial sdk setup

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+export default function sum(...numbers: number[]) {
+    return numbers.reduce((total, number) => total + number, 0)
+}
+
+// can test in development like this:
+//
+// if (import.meta.vitest) {
+//     const { describe, expect, it} = import.meta.vitest
+
+//     describe("#someTest", ()=>{
+//         it("returns 0 with no number parameters", () => {
+//             expect(sum()).toBe(0)
+//         })
+    
+//         it("returns same number with single number provided", () => {
+//             expect(sum(96)).toBe(96)
+//         })
+    
+//         it("returns sum when multiple numbers provided", () => {
+//             expect(sum(1, 2, 3, 4, 5)).toBe(15)
+//         })
+//     })  
+// }
