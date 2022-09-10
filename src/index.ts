23360bbfa767ccc21a41805d6b126d85cf4a3d2f
Harvel218 <gacekrzysztof.218@gmail.com>
2022-09-10 03:22:23 +0200

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,18 @@
-export default function sum(...numbers: number[]) {
-    console.log('dupa123 1234123')
-    return numbers.reduce((total, number) => total + number, 0)
+import SyliusConfig, {baseConfig} from "./config"
+
+import TestService from "./services/testService"
+
+class Sylius {
+    public config: SyliusConfig
+    
+    public test: TestService
+
+    constructor(providedConfig?: baseConfig){
+        this.config = new SyliusConfig(providedConfig)
+
+        this.test = new TestService(this.config)
+    }
+
 }
 
-// can test in development like this:
-//
-// if (import.meta.vitest) {
-//     const { describe, expect, it} = import.meta.vitest
-
-//     describe("#someTest", ()=>{
-//         it("returns 0 with no number parameters", () => {
-//             expect(sum()).toBe(0)
-//         })
-    
-//         it("returns same number with single number provided", () => {
-//             expect(sum(96)).toBe(96)
-//         })
-    
-//         it("returns sum when multiple numbers provided", () => {
-//             expect(sum(1, 2, 3, 4, 5)).toBe(15)
-//         })
-//     })  
-// }
+export default Sylius
---
+++ tests/units/index.test.ts
@@ -0,0 +1,11 @@
+import Sylius from "../../src/index";
+import { describe, expect, isFirstRun, it} from "vitest"
+
+describe("test1", ()=>{
+    it("run test class", async () => {
+        const SDK = new Sylius();
+
+        await SDK.test.testMethod('/locales')
+    })
+
+})
